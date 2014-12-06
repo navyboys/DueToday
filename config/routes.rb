1d95fd1358155eeccbc4d2a648f4db7aa5700aaa
navy <navyboys@gmail.com>
2014-12-06 09:48:52 -0800

    change ABC size metric value for rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#front'
   get 'home', to: 'todos#today'
-  
+
   get 'sign_up', to: "users#new"
   get 'sign_in', to: "sessions#new"
   get 'sign_out', to: "sessions#destroy"
