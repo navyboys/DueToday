506f4f116d51ffccb27dba388a32b79f0a1ab0ad
navy <navyboys@gmail.com>
2014-12-08 22:43:18 -0800

    design todo's controller spec

---
+++ spec/controllers/todos_controller_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe TodosController, type: :controller do
+  describe 'GET index_today' do
+    it 'sets @todos_today to todos due to today of the logged in user'
+    it 'requires sign in'
+  end
+
+  describe 'POST create' do
+    context 'with valid input' do
+      it 'creates a todo'
+      it 'creates a todo accociated with the signed in user'
+    end
+
+    context 'with invalid input' do
+      it 'does not create a todo'
+      it 'shows the error message'
+    end
+
+    it 'requires sign in'
+  end
+
+  describe 'DELETE destroy' do
+    it 'redirects to original page: index_today or index_previous_day'
+    it 'deletes the todo'
+    it 'requires sign in'
+  end
+
+  describe 'GET index_previous_day' do
+    it 'sets @todos_previous_day to todos due to the previous day of the logged in user'
+    it 'requires sign in'
+  end
+
+  describe 'POST update_status' do
+    it "changes todo's status with params"
+    it 'refresh the index_previous_day page'
+    it 'requires sign in'
+  end
+
+  describe 'POST update_summary' do
+    it "changes todo's summary & rate with params"
+    it 'redirects to the index_today page'
+    it 'requires sign in'
+  end
+end
