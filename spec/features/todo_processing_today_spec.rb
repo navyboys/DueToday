e500bd4243172641e0fe5de15e1647a018f989d8
navy <navyboys@gmail.com>
2015-01-24 18:20:19 -0800

    add comments to feature test

--- spec/features/todo_processing_today_spec.rb
+++ spec/features/todo_processing_today_spec.rb
@@ -6,17 +6,21 @@ feature 'todo processing: today' do
     visit_clinic = Fabricate(:todo, user: navy)
     buy_ticket = Fabricate(:todo, user: navy)
 
+    # show todos
     sign_in(navy)
     expect(page).to have_content visit_clinic[:title]
     expect(page).to have_content buy_ticket[:title]
 
+    # add a todo
     fill_in 'Title', with: 'Cook Dinner'
     click_button 'Add'
     expect(page).to have_content 'Cook Dinner'
 
+    # complete(check) a todo
     find("a[href='/todos/#{visit_clinic.id}.completed']").click
     expect(page).to have_css('strike')
 
+    # delete a todo
     find("a[href='/todos/#{buy_ticket.id}']").click
     expect(page).to have_no_content buy_ticket[:title]
   end
