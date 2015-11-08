e0829112c767ef666cf8301d63db8a1dd44913f6
navy <navyboys@gmail.com>
2015-11-07 16:02:29 -0800

    add default url option to production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,5 @@ Rails.application.configure do
     :authentication => :plain,
   }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'duetoday.herokuapp.com' }
 end
