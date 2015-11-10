c65e72a3d86d23e96b0ba1902e4b9e9457086507
navy <navyboys@gmail.com>
2015-11-10 10:05:25 -0800

    use delay to send forgot password email

--- app/controllers/forgot_passwords_controller.rb
+++ app/controllers/forgot_passwords_controller.rb
@@ -3,7 +3,7 @@ class ForgotPasswordsController < ApplicationController
     user = User.where(email: params[:email]).first
     if user
       user.update_column(:token, SecureRandom.urlsafe_base64)
-      AppMailer.send_forgot_password(user).deliver
+      AppMailer.delay.send_forgot_password(user)
       redirect_to forgot_password_confirmation_path
     else
       flash[:error] = params[:email].blank? ? 'Email cannot be blank.' : 'There is no user with that email in the system.'
