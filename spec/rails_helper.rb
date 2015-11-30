dc9acadeb7e10f3ca90105e44355455333ecb27a
navy <navyboys@gmail.com>
2015-11-29 21:30:53 -0800

    remove VCR.config.ignore_localhost = true in rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,7 +30,6 @@ VCR.configure do |config|
   config.cassette_library_dir = 'spec/cassettes'
   config.hook_into :webmock
   config.configure_rspec_metadata!
-  config.ignore_localhost = true
 end
 
 RSpec.configure do |config|
