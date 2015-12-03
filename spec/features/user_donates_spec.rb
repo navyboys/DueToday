bf1bef20b00be2710dd88e151be91f632e4d9cf3
navy <navyboys@gmail.com>
2015-12-03 15:49:17 -0800

    use page.find instead of page.driver.browser.find_element in feature test

--- spec/features/user_donates_spec.rb
+++ spec/features/user_donates_spec.rb
@@ -12,11 +12,16 @@ feature 'User donates' do
 
     stripe_iframe = all('iframe[name=stripe_checkout_app]').last
     Capybara.within_frame stripe_iframe do
-      4.times { page.driver.browser.find_element(:id, 'card_number').send_keys('4242') }
-      page.driver.browser.find_element(:id, 'cc-exp').send_keys '5'
-      page.driver.browser.find_element(:id, 'cc-exp').send_keys '18'
-      page.driver.browser.find_element(:id, 'cc-csc').send_keys '123'
-      page.driver.browser.find_element(:id, 'submitButton').click
+      4.times { page.find('#card_number').native.send_keys('4242') }
+      page.find('#cc-exp').native.send_keys('12')
+      page.find('#cc-exp').native.send_keys('20')
+      page.find('#cc-csc').native.send_keys('123')
+      page.find('#submitButton').click
+      # 4.times { page.driver.browser.find_element(:id, 'card_number').send_keys('4242') }
+      # page.driver.browser.find_element(:id, 'cc-exp').send_keys '5'
+      # page.driver.browser.find_element(:id, 'cc-exp').send_keys '18'
+      # page.driver.browser.find_element(:id, 'cc-csc').send_keys '123'
+      # page.driver.browser.find_element(:id, 'submitButton').click
     end
     sleep(3)
     expect(page).to have_content 'THANK YOU'
